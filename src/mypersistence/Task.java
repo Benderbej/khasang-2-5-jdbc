ddaed5d842a6f46cd931ee1cd61e98e1c0aef1d9
Ostap Strashevskii <strashevskyseo@gmail.com>
2019-02-04 11:09:18 +0300

    third

--- src/mypersistence/Task.java
+++ src/mypersistence/Task.java
@@ -1,8 +1,12 @@
 package mypersistence;
 
 import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 
 public class Task {
@@ -12,61 +16,46 @@ public class Task {
     private Timestamp startTimeStamp;
     private long startTime;
     private long lifeTimeLong;//long
+    public static final DateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzzz yyyy", Locale.ENGLISH);
 
     public static void main(String[] args) {
-
-
         Calendar c = Calendar.getInstance();
         c.set(2005, 4, 5);//текущая дата подменяется подставной
         Date today = c.getTime();
         System.out.println(today);
-
         c.set(2005, 4, 6);
         Date startDay = c.getTime();
         System.out.println(startDay);
-
-
         Task task = new Task();
         boolean b = task.checkIfExpired(today, startDay, 86500l);//86500 sometime more than 24h
-
-
-    }
-
-    public boolean checkIfExpired(Date today, String startTimestampString, long time){
-        //java.util.Date today = new java.util.Date();
-        Timestamp todayTime = new Timestamp(today.getTime());
-        long todayTimeSeconds = todayTime.getTime();
-        System.out.println("todayTimeSeconds"+todayTimeSeconds);
-
-        Timestamp startTime = Timestamp.valueOf(startTimestampString);
-        long startTimeSeconds = startTime.getTime();
-
-        System.out.println("startTimeSeconds"+startTimeSeconds);
-        long expireTimeSeconds = startTimeSeconds + time;
-
-        System.out.println("expireTimeSeconds"+expireTimeSeconds);
-        if(todayTimeSeconds > expireTimeSeconds){
-            return true;
-        }
-        return false;
     }
 
     public boolean checkIfExpired(Date today, Date startDay, long time ){
         Timestamp todayTime = new Timestamp(today.getTime());
         long todayTimeSeconds = todayTime.getTime();
-        System.out.println("todayTimeSeconds"+todayTimeSeconds);
-
         Timestamp startTime = new Timestamp(startDay.getTime());
         long startTimeSeconds = startTime.getTime();
-        System.out.println("startTimeSeconds"+startTimeSeconds);
-
         long expireTimeSeconds = startTimeSeconds + time;
-        System.out.println("expireTimeSeconds"+expireTimeSeconds);
-
         if(todayTimeSeconds > expireTimeSeconds){
             return true;
         }
         return false;
+    }
+
+    public Date stringToDate(String s){
+        Date date = null;
+        try {
+            date = format.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.out.println("date format is not valid, and cant be parsed");
+        }
+        return date;
+    }
+
+    public String dateToString(Date d){
+        String reportDate = format.format(d);
+        return reportDate;
     }
 
     public int getId() {
--- test/mypersistence/TaskTest.java
+++ test/mypersistence/TaskTest.java
@@ -9,35 +9,17 @@ import java.util.Date;
 import static org.junit.Assert.*;
 
 public class TaskTest {
-    @Test
-    public void checkIfExpired() throws Exception {
-        //текущая дата подменяется подставной
-        //java.util.Date today = new java.util.Date();
-        java.util.Date today = new java.util.Date("2005-04-06 09:01:10");
-        Task task = new Task();
-        boolean b = task.checkIfExpired(today, "2005-04-05 09:01:10", 86500l);//86500 sometime more than 24h
-        Assert.assertTrue(!b);
-        //boolean b2 = task.checkIfExpired(today, "2005-04-04 09:01:10", 86500l);//86500 sometime more than 24h
-        //Assert.assertTrue(b2);
-    }
 
     @Test
     public void checkIfExpiredTest() throws Exception {
-
-
-
         Calendar c = Calendar.getInstance();
         c.set(2005, 4, 5);//текущая дата подменяется подставной
         Date today = c.getTime();
-
         c.set(2005, 4, 6);
         Date startDay = c.getTime();
-
-
         Task task = new Task();
         boolean b = task.checkIfExpired(today, startDay, 86500l);//86500 sometime more than 24h
         Assert.assertTrue(!b);
-
     }
 
     @Test
@@ -59,5 +41,4 @@ public class TaskTest {
     @Test
     public void getLifeTimeLong() throws Exception {
     }
-
 }
